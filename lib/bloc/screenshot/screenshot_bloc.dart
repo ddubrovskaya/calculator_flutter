8f8380bee2ea6e4151670b653da8d9e1b95c9a16
Daria Dubrovskaya <dariadubrovskaya@coherentsolutions.com>
2024-10-26 00:39:20 +0300

    Created Advanced: Sensitive information

---
+++ lib/bloc/screenshot/screenshot_bloc.dart
@@ -0,0 +1,52 @@
+import 'package:calculator_flutter/bloc/screenshot/screenshot_event.dart';
+import 'package:calculator_flutter/bloc/screenshot/screenshot_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:ui' as ui;
+import 'package:logger/logger.dart';
+
+class ScreenshotBloc extends Bloc<ScreenshotEvent, ScreenshotState> {
+  final GlobalKey boundaryKey;
+  final logger = Logger();
+
+  ScreenshotBloc(this.boundaryKey) : super(ScreenshotInitial()) {
+    on<CaptureScreenshot>(_onCaptureScreenshot);
+    on<ShowScreenshot>(_onShowScreenshot);
+    on<HideScreenshot>(_onHideScreenshot);
+    on<ShowMask>(_onShowMask);
+    on<HideMask>(_onHideMask);
+  }
+
+  Future<void> _onCaptureScreenshot(
+      CaptureScreenshot event, Emitter<ScreenshotState> emit) async {
+    try {
+      RenderRepaintBoundary boundary = boundaryKey.currentContext
+          ?.findRenderObject() as RenderRepaintBoundary;
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+      emit(ScreenshotCaptured(image));
+    } catch (e) {
+      logger.e("Error capturing image: $e");
+    }
+  }
+
+  void _onShowScreenshot(ShowScreenshot event, Emitter<ScreenshotState> emit) {
+    if (state is ScreenshotCaptured) {
+      emit(ScreenshotDisplayed((state as ScreenshotCaptured).image));
+    }
+  }
+
+  void _onHideScreenshot(HideScreenshot event, Emitter<ScreenshotState> emit) {
+    emit(ScreenshotInitial());
+  }
+
+  void _onShowMask(ShowMask event, Emitter<ScreenshotState> emit) {
+    print('ShowMask event received');
+    emit(MaskVisibleState());
+  }
+
+  void _onHideMask(HideMask event, Emitter<ScreenshotState> emit) {
+    print('HideMask event received');
+    emit(ScreenshotInitial());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
 import 'package:calculator_flutter/bloc/carousel/carousel_bloc.dart';
 import 'package:calculator_flutter/bloc/navigation/navigation_bloc.dart';
+import 'package:calculator_flutter/bloc/screenshot/screenshot_bloc.dart';
+import 'package:calculator_flutter/bloc/screenshot/screenshot_event.dart';
+import 'package:calculator_flutter/bloc/screenshot/screenshot_state.dart';
 import 'package:calculator_flutter/bloc/table/table.bloc.dart';
 import 'package:calculator_flutter/bloc/webview/webview_bloc.dart';
 import 'package:calculator_flutter/home_screen.dart';
@@ -11,13 +15,83 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  late ScreenshotBloc _screenshotBloc;
+  final GlobalKey _boundaryKey = GlobalKey();
+  bool _showMask = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _screenshotBloc = ScreenshotBloc(_boundaryKey);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _screenshotBloc.close();
+    super.dispose();
+  }
+
+  // @override
+  // void didChangeAppLifecycleState(AppLifecycleState state) {
+  //   if (state == AppLifecycleState.paused) {
+  //     _screenshotBloc.add(CaptureScreenshot());
+  //   } else if (state == AppLifecycleState.resumed) {
+  //     _screenshotBloc.add(ShowScreenshot());
+  //     Timer(const Duration(seconds: 4), () {
+  //       _screenshotBloc.add(HideScreenshot());
+  //     });
+  //   }
+  // }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      setState(() {
+        _showMask = true;
+      });
+      _screenshotBloc.add(CaptureScreenshot());
+    } else if (state == AppLifecycleState.resumed) {
+      _screenshotBloc.add(ShowScreenshot());
+      Timer(const Duration(seconds: 4), () {
+        _screenshotBloc.add(HideScreenshot());
+        setState(() {
+          _showMask = true;
+        });
+      });
+    }
+  }
+
+//   @override
+// void didChangeAppLifecycleState(AppLifecycleState state) {
+//   if (state == AppLifecycleState.paused) {
+//     _screenshotBloc.add(ShowMask()); // Показать маску
+//     _screenshotBloc.add(CaptureScreenshot());
+//   } else if (state == AppLifecycleState.resumed) {
+//     _screenshotBloc.add(ShowScreenshot());
+//     Timer(const Duration(seconds: 4), () {
+//       _screenshotBloc.add(HideScreenshot());
+//       _screenshotBloc.add(HideMask()); // Скрыть маску
+//     });
+//   }
+// }
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider<ScreenshotBloc>(
+          create: (_) => _screenshotBloc,
+        ),
         BlocProvider<CalculatorBloc>(
           create: (context) => CalculatorBloc(),
         ),
@@ -35,7 +109,38 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        home: HomeScreen(),
+        home: BlocBuilder<ScreenshotBloc, ScreenshotState>(
+          builder: (context, state) {
+            if (state is ScreenshotDisplayed) {
+              return Scaffold(
+                body: RepaintBoundary(
+                  key: _boundaryKey,
+                  child: Stack(
+                    children: [
+                      RawImage(image: state.image),
+                      if (_showMask)
+                        Positioned(
+                          top: 400,
+                          left: 20,
+                          child: Container(
+                            width: 350,
+                            height: 350,
+                            color: Colors.black,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            }
+            return Scaffold(
+              body: RepaintBoundary(
+                key: _boundaryKey,
+                child: HomeScreen(),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
